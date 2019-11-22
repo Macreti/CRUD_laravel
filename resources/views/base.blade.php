b11de688e92ec096016c24b26613b206acdc1bac
macreti <gitmacreti@gmail.com>
2019-11-22 03:19:05 +0100

    CRUD using laravel

---
+++ resources/views/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD Tutorial using Laravel</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container" style="background-color:lightgrey; padding:2% 0%;">
+    @yield('content')
+  </div>
+  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+</body>
